8461aee46f46a7d752ef4919b68e77a4316f7d61
彭焱东 <939308479@qq.com>
2018-07-13 11:40:36 +0800

    完成定时任务2.0

--- src/main/java/com/jufan/Timer/TimedTask.java
+++ src/main/java/com/jufan/Timer/TimedTask.java
@@ -29,105 +29,105 @@ public class TimedTask {
     }
 
 
-//    /**
-//     *  每个小时将query和jf表里的数据拉到新的拓展表里
-//     */
-//    public void queryDataByAnHour() {
-//        System.out.println("进入定时任务，每隔一个小时拉取数据");
-//        Calendar ca = Calendar.getInstance();
-//        ca.set(Calendar.MINUTE, 0);
-//        ca.set(Calendar.SECOND, 0);
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
-//        String suffix=sdf1.format(new Date());
-//        //当前时间小时整点
-//        Date endDate = ca.getTime();
-//        String endTime=sdf.format(endDate);
-//        System.out.println("当前时间整点小时"+endTime);
-//
-//        ca.set(Calendar.HOUR_OF_DAY, ca.get(Calendar.HOUR_OF_DAY)-1);
-//        Date startDate = ca.getTime();
-//        String startTime=sdf.format(startDate);
-//        System.out.println("当前时间前一小时整点"+startTime);
-//
-//        Map<String,Object> map = new HashMap<String,Object>();
-//        map.put("startTime",startTime);
-//        map.put("endTime",endTime);
-//
-//        List<PdopJfReqlog> pdopJfReqlogList= pdopJfReqlogService.selectByHour(map);
-//        List<PdopQueryLog> pdopQueryLogList= pdopQueryLogService.selectByHour(map);
-//        if(pdopJfReqlogList.size()>0&&pdopQueryLogList.size()>0){
-//            String jfTableName="pdop_data_jfext_"+suffix;
-//            String queryTableName="pdop_data_queryext_"+suffix;
-//
-//            try {
-//                Boolean jf =tableManagerService.checkTable(jfTableName);
-//                Boolean query=tableManagerService.checkTable(queryTableName);
-//
-//                if(query==true){
-//                    pdopQueryLogService.insertQueryExtList(queryTableName,pdopQueryLogList);
-//                }else {
-//                    ReturnData returnData = tableManagerService.createQueryTable(queryTableName);
-//                    if (returnData.getCode().equals("OK")) {
-//                        pdopQueryLogService.insertQueryExtList(queryTableName, pdopQueryLogList);
-//                    }
-//                }
-//
-//                if(jf==true){
-//                    pdopJfReqlogService.insertJfExtList(jfTableName,pdopJfReqlogList);
-//                }else{
-//
-//                    ReturnData returnData1=tableManagerService.createJfTable(jfTableName);
-//                    if(returnData1.getCode().equals("OK")){
-//                        pdopJfReqlogService.insertJfExtList(jfTableName,pdopJfReqlogList);
-//                    }
-//                }
-//
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//
-//        }
-//
-//    }
-//
-//
-//    /**
-//     *     每个月初删掉三个月之前的拓展表表  每月1号1点执行
-//     */
-//    public void dropTableByMonth() {
-//       //获取当前月的时间
-//        Calendar ca = Calendar.getInstance();
-//        ca.set(Calendar.MINUTE, 0);
-//        ca.set(Calendar.SECOND, 0);
-//        ca.set(Calendar.MONTH, ca.get(Calendar.MONTH)-3);
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
-//        String suffix=sdf.format(ca.getTime());
-//        System.out.println(suffix);
-//        String jfTableName="pdop_data_jfext_"+suffix;
-//        String queryTableName="pdop_data_queryext_"+suffix;
-//
-//        try {
-//            Boolean jf =tableManagerService.checkTable(jfTableName);
-//            Boolean query=tableManagerService.checkTable(queryTableName);
-//
-//            if(query==true){
-//                tableManagerService.dropTable(queryTableName);
-//            }
-//
-//            if(jf==true){
-//                tableManagerService.dropTable(jfTableName);
-//            }
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//
-//
-//
-//
-//
-//    }
+    /**
+     *  每个小时将query和jf表里的数据拉到新的拓展表里
+     */
+    public void queryDataByAnHour() {
+        System.out.println("进入定时任务，每隔一个小时拉取数据");
+        Calendar ca = Calendar.getInstance();
+        ca.set(Calendar.MINUTE, 0);
+        ca.set(Calendar.SECOND, 0);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        String suffix=sdf1.format(new Date());
+        //当前时间小时整点
+        Date endDate = ca.getTime();
+        String endTime=sdf.format(endDate);
+        System.out.println("当前时间整点小时"+endTime);
+
+        ca.set(Calendar.HOUR_OF_DAY, ca.get(Calendar.HOUR_OF_DAY)-1);
+        Date startDate = ca.getTime();
+        String startTime=sdf.format(startDate);
+        System.out.println("当前时间前一小时整点"+startTime);
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("startTime",startTime);
+        map.put("endTime",endTime);
+
+        List<PdopJfReqlog> pdopJfReqlogList= pdopJfReqlogService.selectByHour(map);
+        List<PdopQueryLog> pdopQueryLogList= pdopQueryLogService.selectByHour(map);
+        if(pdopJfReqlogList.size()>0&&pdopQueryLogList.size()>0){
+            String jfTableName="pdop_data_jfext_"+suffix;
+            String queryTableName="pdop_data_queryext_"+suffix;
+
+            try {
+                Boolean jf =tableManagerService.checkTable(jfTableName);
+                Boolean query=tableManagerService.checkTable(queryTableName);
+
+                if(query==true){
+                    pdopQueryLogService.insertQueryExtList(queryTableName,pdopQueryLogList);
+                }else {
+                    ReturnData returnData = tableManagerService.createQueryTable(queryTableName);
+                    if (returnData.getCode().equals("OK")) {
+                        pdopQueryLogService.insertQueryExtList(queryTableName, pdopQueryLogList);
+                    }
+                }
+
+                if(jf==true){
+                    pdopJfReqlogService.insertJfExtList(jfTableName,pdopJfReqlogList);
+                }else{
+
+                    ReturnData returnData1=tableManagerService.createJfTable(jfTableName);
+                    if(returnData1.getCode().equals("OK")){
+                        pdopJfReqlogService.insertJfExtList(jfTableName,pdopJfReqlogList);
+                    }
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+    /**
+     *     每个月初删掉三个月之前的拓展表表  每月1号1点执行
+     */
+    public void dropTableByMonth() {
+       //获取当前月的时间
+        Calendar ca = Calendar.getInstance();
+        ca.set(Calendar.MINUTE, 0);
+        ca.set(Calendar.SECOND, 0);
+        ca.set(Calendar.MONTH, ca.get(Calendar.MONTH)-3);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        String suffix=sdf.format(ca.getTime());
+        System.out.println(suffix);
+        String jfTableName="pdop_data_jfext_"+suffix;
+        String queryTableName="pdop_data_queryext_"+suffix;
+
+        try {
+            Boolean jf =tableManagerService.checkTable(jfTableName);
+            Boolean query=tableManagerService.checkTable(queryTableName);
+
+            if(query==true){
+                tableManagerService.dropTable(queryTableName);
+            }
+
+            if(jf==true){
+                tableManagerService.dropTable(jfTableName);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
 
 
 
