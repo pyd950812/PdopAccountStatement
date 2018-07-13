8ec0335788c0a5833ede9d088acd1f1aebfc6141
pyd950812 <pyd939308479>
2018-07-13 15:32:39 +0800

    新增账单查询业务

--- src/main/java/com/jufan/service/MerchantAccountService.java
+++ src/main/java/com/jufan/service/MerchantAccountService.java
@@ -2,6 +2,10 @@ package com.jufan.service;
 
 public interface MerchantAccountService {
 
+    /**
+     * 获取渠道月账单的详细信息
+     * @param orgId
+     */
     public void getMerchantCountByOrgId(String orgId);
 
 
