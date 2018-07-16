f3ea8fa5f845f387cb9cc262745d1013b02c347b
pyd950812 <pyd939308479>
2018-07-16 10:46:31 +0800

    添加注解

--- src/main/java/com/jufan/dao/MerchantAccountDao.java
+++ src/main/java/com/jufan/dao/MerchantAccountDao.java
@@ -8,9 +8,13 @@ import java.util.List;
 import java.util.Map;
 @Repository
 public interface MerchantAccountDao {
-
+     /**
+      * 根据jf和query拓展表的名字和商户id查询产品id和产品月调用次数
+      */
      List<Map<String,Object >> getAccountDetailByOrgId(Map<String,Object> map);
-
+     /**
+      * 根据产品id查询产品的价格和名字
+      */
      Map<String,Object>  getProductNameById(@Param("id") String id);
 
 }
